500d0342552b31e9369befb25865126af4654dd9
tdascoli <R0hrch@ch>
2022-10-02 20:25:25 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp        = require('gulp');
-var sass        = require('gulp-sass');
+var sass        = require('gulp-sass')(require('sass'));
 var minify      = require('gulp-minifier');
 var zip         = require('gulp-zip');
 var file        = require('gulp-file');
