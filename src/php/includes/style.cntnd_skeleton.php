08a53037710827facc91e76ec143070088621b04
tdascoli <R0hrch@ch>
2023-02-28 13:16:17 +0100

    update

--- src/php/includes/style.cntnd_skeleton.php
+++ src/php/includes/style.cntnd_skeleton.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/cntnd/core_style@1.1.1/dist/core_style.css">
 <style>
-<?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_SKELETON/css/cntnd_SKELETON.css') ?>
+<?php
+$mod = new cApiModule($cCurrentModule);
+echo file_get_contents($cfgClient[$client]["module"]["path"].'/'.$mod->get("alias").'/css/'.$mod->get("alias").'.css')
+?>
 </style>
